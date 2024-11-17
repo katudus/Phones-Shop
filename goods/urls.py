a3aa11acd8f4a1101b048374723cf0b52523572f
KATUDUS <katudus@yandex.ru>
2024-11-18 00:29:11 +0300

    Simple search create

--- goods/urls.py
+++ goods/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from goods import views
 
-app_name = "goods"
+app_name = 'goods'
 
 urlpatterns = [
-    path("<slug:category_slug>/", views.catalog, name="index"),
-    path("product/<slug:product_slug>/", views.product, name="product"),
+    path('search/', views.catalog, name='search'),
+    path('<slug:category_slug>/', views.catalog, name='index'),
+    path('product/<slug:product_slug>/', views.product, name='product'),
 ]
