c9e2030a6ec185b0f2aa006b87cb095ee357c3ec
KATUDUS <katudus@yandex.ru>
2024-11-26 02:29:56 +0300

    Create order is ready

--- orders/forms.py
+++ orders/forms.py
@@ -6,19 +6,19 @@ class CreateOrderForm(forms.Form): # свой класс формы полнос
     first_name = forms.CharField()
     last_name = forms.CharField()
     phone_number = forms.CharField()
-    requires_delivery = forms.ChoiceField()
-        # choices=[
-        #     ("0", False),
-        #     ("1", True),
-        #     ],
-        # )
+    requires_delivery = forms.ChoiceField(
+        choices=[
+            ("0", False),
+            ("1", True),
+            ],
+        )
     delivery_address = forms.CharField(required=False)
-    payment_on_get = forms.ChoiceField()
-    #     choices=[
-    #         ("0", 'False'),
-    #         ("1", 'True'),
-    #         ],
-    #     )
+    payment_on_get = forms.ChoiceField(
+        choices=[
+            ("0", 'False'),
+            ("1", 'True'),
+            ],
+        )
 
     # def clean_phone_number(self):
     #     data = self.cleaned_data['phone_number']
