a031d0bbde8191c246429a003fe2011cb6b03acd
KATUDUS <katudus@yandex.ru>
2024-11-25 22:51:50 +0300

    Sessions - carts for anonymous user

--- carts/utils.py
+++ carts/utils.py
@@ -4,3 +4,7 @@ from carts.models import Cart
 def get_user_carts(request):
     if request.user.is_authenticated:
         return Cart.objects.filter(user=request.user) # выбираем все корзины авторизир юзера
+
+    if not request.session.session_key:
+        request.session.create() # сессионнный ключ для анонимного юзера
+    return Cart.objects.filter(session_key=request.session.session_key)
--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from carts.models import Cart
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
@@ -14,10 +15,15 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password) # проверка наличия пользователя, возв юзера
+
+            session_key = request.session.session_key
+
             if user:
                 auth.login(request, user) # авторизуем
                 messages.success(request, f"{username}, Вы вошли в аккаунт") # обратная связь с пользователем
 
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
                 redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next')) # перенаправление на страницу которую вблил юзер в url но для которой нужен вход в акк
@@ -38,9 +44,15 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save() # сохранение в бд нового юзера
+
+            session_key = request.session.session_key
+
             user = form.instance # получение пользователя из формы
             auth.login(request, user) # обход повторного ввода логина и пароля
-            messages.success(request, f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт")
+
+            if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
+            messages.success(request, f"{user.username}, вы успешно зарегистрированы и вошли в аккаунт")
             return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrationForm() # пустая форма
