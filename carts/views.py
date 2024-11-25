13db6203894fe3ee9f6efee5937c6fe516d8e8ff
KATUDUS <katudus@yandex.ru>
2024-11-25 21:20:49 +0300

    Add-to-cart ajax request and json response

--- carts/views.py
+++ carts/views.py
@@ -1,12 +1,17 @@
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 
 from carts.models import Cart
+from carts.utils import get_user_carts
 from goods.models import Products
 
 # Create your views here.
-def cart_add(request, product_slug):
-    
-    product = Products.objects.get(slug=product_slug) # получаем экземпляр продукта добавленного в корзину юзером
+def cart_add(request):
+
+    product_id = request.POST.get('product_id')
+
+    product = Products.objects.get(id=product_id) # получаем экземпляр продукта добавленного в корзину юзером
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product) # наличие такого же продукта в его корзине
@@ -19,7 +24,17 @@ def cart_add(request, product_slug):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1) # создаем корзину юзеру по этому товару
 
-    return redirect(request.META['HTTP_REFERER']) # возврат на ту же страницу с которой запрос
+    user_cart = get_user_carts(request)
+    
+    cart_items_html = render_to_string(
+        'carts/includes/included_cart.html', {'carts': user_cart}, request=request)
+
+    response_data = {
+        'message': 'Товар добавлен в корзину',
+        'cart_items_html': cart_items_html,
+    }
+
+    return JsonResponse(response_data) # перерисовка js той же страницы с которой запрос
 
 
 def cart_change(request, product_slug):
